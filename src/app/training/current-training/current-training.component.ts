d25598c228ec40a4bbb0d8cef4bd0d5906514a40
Andrzej Szelemetko <a.szelemekto@gmail.com>
2018-12-17 18:13:35 +0100

    further training service development

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {StopTrainingComponent} from './stop-training.component';
+import {TrainingService} from '../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -14,7 +15,7 @@ export class CurrentTrainingComponent implements OnInit {
   timer: number;
 
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog, private trainingSercie: TrainingService) {
   }
 
   ngOnInit(): void {
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,14 +1,24 @@
 import {Exercise} from './exercise.module';
+import {Subject} from 'rxjs';
 
 export class TrainingService {
-  availableExervises: Exercise[] = [
+  availableExercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
+  private currentExercise: Exercise;
+  exerciseChanged = new Subject<Exercise>();
 
   getAvailableExercises() {
-    return this.availableExervises.slice();
+    return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.currentExercise = this.availableExercises.find(ex => {
+      return ex.id === selectedId;
+    });
+    this.exerciseChanged.next({...this.currentExercise});
   }
 }
