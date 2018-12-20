aeeef5be105f16e80cd7b08441da224f877f23a2
Andrzej Szelemetko <a.szelemetko@gmail.com>
2018-12-20 14:58:03 +0100

    moved auth to ngrx store

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,22 +1,23 @@
-import {CanLoad, Route, Router, UrlSegment} from '@angular/router';
+import {CanLoad, Route, UrlSegment} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
-import {AuthService} from './auth.service';
+import {Store} from '@ngrx/store';
+
+import * as fromRoot from '../app.reducer';
+import {take} from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanLoad {
 
   constructor(
-    private authService: AuthService,
-    private router: Router
+    private store: Store<fromRoot.State>
   ) {
   }
 
   canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isAuth()) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-    }
+    return this.store.select(fromRoot.getIsAuthenticated)
+      .pipe(
+        take(1)
+      );
   }
 }
